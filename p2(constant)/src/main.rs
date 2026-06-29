804a96c6fd5fb1507a28515f531f30c558610329
notarayaoranurag <notarayaoranurag@gmail.com>
2026-06-29 23:50:00 +0530

    Add files via upload

---
+++ p2(constant)/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    const DOB: i32 = 23092009; //to create a constant you just need to replace let with const it is immutable
+    // const DOB = 23092009; will likely cause a error cause inconstant you need to define data types
+}
